088921c5c2c5904db30c09fb0bced528019eabd5
siddhi0092 <siddhirajput1980@gmail.com>
2026-03-04 22:25:38 +0530

    first commit

---
+++ prob-2184-A.py
@@ -0,0 +1,23 @@
+# t = int(input())
+
+# for _ in range(t):
+#     n = int(input())
+    
+#     if n == 2:
+#         print(2)
+#     elif n % 2 == 0:
+#         print(0)
+#     else:
+#         print(1)
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    
+    if n <= 3:
+        print(n)
+    elif n % 2 == 0:
+        print(0)
+    else:
+        print(1)
